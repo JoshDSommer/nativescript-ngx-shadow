316c19461aa5733ef192f5ff4ccad7efe3f6d2d6
Eduardo Speroni <edusperoni@gmail.com>
2019-04-06 01:51:58 -0300

    monkey patch is no longer needed

--- lib/src/nativescript-ngx-shadow/ng-shadow.directive.ts
+++ lib/src/nativescript-ngx-shadow/ng-shadow.directive.ts
@@ -47,11 +47,7 @@ export class NativeShadowDirective implements OnInit, OnChanges, AfterViewInit, 
   private iosShadowRapper: View;
   private eventsBound = false;
 
-  constructor(private el: ElementRef, private render: Renderer2) {
-    if (isAndroid) {
-      this.originalNSFn = this.el.nativeElement._redrawNativeBackground; //always store the original method
-    }
-  }
+  constructor(private el: ElementRef, private render: Renderer2) { }
 
   ngOnInit() { // RadListView calls this multiple times for the same view
     if (!this.initialized) {
@@ -117,10 +113,6 @@ export class NativeShadowDirective implements OnInit, OnChanges, AfterViewInit, 
       this.ngOnInit();
     }
     this.applyShadow();
-    if (isAndroid) {
-      this.previousNSFn = this.el.nativeElement._redrawNativeBackground; // just to maintain compatibility with other patches
-      this.el.nativeElement._redrawNativeBackground = this.monkeyPatch;
-    }
   }
 
   private addIosWrapper() {
@@ -141,10 +133,6 @@ export class NativeShadowDirective implements OnInit, OnChanges, AfterViewInit, 
 
   onUnloaded() {
     this.loaded = false;
-
-    if (isAndroid) {
-      this.el.nativeElement._redrawNativeBackground = this.originalNSFn; // always revert to the original method
-    }
   }
   ngAfterViewInit() {
     this.addIosWrapper();
@@ -188,11 +176,6 @@ export class NativeShadowDirective implements OnInit, OnChanges, AfterViewInit, 
       this.applyShadow();
     }
   }
-
-  private monkeyPatch = val => {
-    this.previousNSFn.call(this.el.nativeElement, val);
-    this.applyShadow();
-  };
 
   private applyShadow() {
     if (
